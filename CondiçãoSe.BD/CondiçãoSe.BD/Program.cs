df22c7979d47a6d67ac00d460d659256983d35d1
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:27:53 -0300

    Add files via upload

---
+++ CondiçãoSe.BD/CondiçãoSe.BD/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class Condiçãose
+{
+    public static void Main(string[] args)
+    {
+        string nome;
+        int senha = 1234;
+        string cpf;
+        int idade;
+
+        Console.WriteLine("Digite o seu nome");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a senha");
+        senha = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o seu cpf");
+        cpf = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade");
+        idade = int.Parse(Console.ReadLine());
+
+        if (senha == 1234 && idade >= 18)
+        {
+            Console.WriteLine("Entre!");
+
+        }
+        else {
+            Console.WriteLine("Tente novamente ;(");
+       
+        }
+
+
+
+
+    }
+
+
+
+}
